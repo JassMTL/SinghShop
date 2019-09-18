29180580e1dee0c7759bd23666926148a2f7384b
JassMTL <jaspreet.jst@gmail.com>
2019-09-18 15:59:23 -0400

    programed the increase decrease

--- app/src/main/java/com/example/singhshop/MenuActivity.java
+++ app/src/main/java/com/example/singhshop/MenuActivity.java
@@ -2,12 +2,74 @@ package com.example.singhshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
 
+    private final double priceKarra = 24.99;
+    private final double priceKirpan = 99.99;
+    private final double pricePendant = 19.99;
+
+    private double subtotal = 0;
+
+    private int karraCount = 0;
+    private int kirpanCount = 0;
+    private int pendantCount = 0;
+
+    private TextView showKarraCount;
+    private TextView showKirpanCount;
+    private TextView showPendantCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        showKarraCount = (TextView) findViewById(R.id.amountKarra);
+        showKirpanCount = (TextView) findViewById(R.id.amountKirpan);
+        showPendantCount = (TextView) findViewById(R.id.amountPendant);
+
+    }
+
+    public void increaseKarra(View view){
+        ++karraCount;
+        showKarraCount.setText(Integer.toString(karraCount));
+        subtotal +=priceKarra;
+    }
+
+    public void decreaseKarra(View view){
+        if(karraCount != 0) {
+            --karraCount;
+            showKarraCount.setText(Integer.toString(karraCount));
+            subtotal -=priceKarra;
+        }
+    }
+
+    public void increaseKirpan(View view){
+        ++kirpanCount;
+        showKirpanCount.setText(Integer.toString(kirpanCount));
+        subtotal += priceKirpan;
+    }
+
+    public void decreaseKirpan(View view){
+        if(kirpanCount != 0) {
+            --kirpanCount;
+            showKirpanCount.setText(Integer.toString(kirpanCount));
+            subtotal -= priceKirpan;
+        }
+    }
+
+    public void increasePendant(View view){
+        ++pendantCount;
+        showPendantCount.setText(Integer.toString(pendantCount));
+        subtotal += pricePendant;
+    }
+
+    public void decreasePendant(View view){
+        if(pendantCount != 0) {
+            --pendantCount;
+            showPendantCount.setText(Integer.toString(pendantCount));
+            subtotal -= pricePendant;
+        }
     }
 }
